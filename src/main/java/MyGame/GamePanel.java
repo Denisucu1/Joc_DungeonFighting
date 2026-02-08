78175b9a89ed7c734f789580c1b769570afb789b
Denisucu1 <denisro463@gmail.com>
2026-02-09 01:05:43 +0200

    Adaugare de movement + modificare in gamepanel

--- src/main/java/MyGame/GamePanel.java
+++ src/main/java/MyGame/GamePanel.java
@@ -2,6 +2,7 @@ package MyGame;
 
 import javax.swing.*;
 import java.awt.*;
+import Input.KeyHandler;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -14,12 +15,27 @@ public class GamePanel extends JPanel implements Runnable {
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void update() {
+        if (keyH.upPressed) playerY -= playerSpeed;
+        if (keyH.downPressed) playerY += playerSpeed;
+        if (keyH.leftPressed) playerX -= playerSpeed;
+        if (keyH.rightPressed) playerX += playerSpeed;
     }
 
     public void startGameThread() {
@@ -29,20 +45,24 @@ public class GamePanel extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        while(gameThread != null) {
-            update();
-            repaint();
+        double drawInterval = 1000000000 / 60; // 0.01666 secunde
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
-            try {
-                Thread.sleep(16);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
         }
     }
 
-    public void update() {
-    }
 
     @Override
     public void paintComponent(Graphics g) {
@@ -50,7 +70,7 @@ public class GamePanel extends JPanel implements Runnable {
         Graphics2D g2 = (Graphics2D) g;
 
         g2.setColor(Color.WHITE);
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose();
     }
